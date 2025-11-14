4baf362d4be6025dc9035801f067a046f8542022
wrlaxodt <wrlaxodt@gmail.com>
2025-11-14 11:11:30 +0900

    feat: Add memoized Fibonacci implementation

--- fibo_seq.py
+++ fibo_seq.py
@@ -1,8 +1,10 @@
-def fibo(n):
-    if n < 2:
-        return n
-    else:
-        return fibo(n-1) + fibo(n-2)
-
+def fibo():
+    pad = {0: 0, 1: 1}
+    def fibo_inner(n):
+        if not n in pad:
+            pad[n] = fibo_inner(n - 1) + fibo_inner(n - 2)
+        return pad[n]
+    return fibo_inner
 if __name__ == '__main__':
-    fibo(6)
+    func = fibo()
+    print(func(6))
